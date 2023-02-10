a5f733cf1d2422f6e0edc7a48528a0ef310e14c2
Ismoil <ismoil997@seznam.cz>
2023-02-10 17:37:50 +0500

    reverse integer is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let result;
+  numStr = String(n);
+  for (let i = 0; i < numStr.length; i++) {
+    result = `${numStr[i]}${result}`; 
+  }
+  return parseInt(result);
 }
